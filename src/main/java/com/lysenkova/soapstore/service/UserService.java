73b9b271ada2f80ed7d86d507b3777777885c641
Yana <yana.lysenkova@gmail.com>
2018-05-25 12:54:51 +0300

    UserService implemented

---
+++ src/main/java/com/lysenkova/soapstore/service/UserService.java
@@ -0,0 +1,9 @@
+package com.lysenkova.soapstore.service;
+
+import com.lysenkova.soapstore.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAll();
+}
---
+++ src/main/java/com/lysenkova/soapstore/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.lysenkova.soapstore.service.impl;
+
+import com.lysenkova.soapstore.dao.UserDao;
+import com.lysenkova.soapstore.entity.User;
+import com.lysenkova.soapstore.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    private UserDao userDao;
+    @Override
+    public List<User> getAll() {
+        return userDao.getAll();
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+}
